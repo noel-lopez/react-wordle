4fb2cfc6a9ce069cc2850cd4645197106dbc4807
noel-lopez <nlopez.publi@gmail.com>
2022-12-19 21:36:26 +0100

    added color animations

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -18,7 +18,6 @@ export default function Wordle({ solution }) {
   return (
     <div>
       <div>solution: {solution}</div>
-      <div>current guess: {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
     </div>
   )
